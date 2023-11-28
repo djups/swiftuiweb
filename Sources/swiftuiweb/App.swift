3a6fa9893cabe03753715bb692509ad94f3163d1
Alex Jovmir <ajog@yandex.ru>
2023-11-28 19:04:03 +0100

    test

--- Sources/swiftuiweb/App.swift
+++ Sources/swiftuiweb/App.swift
@@ -17,46 +17,51 @@ struct TokamakApp: App {
 }
 
 struct Counter: View {
-  @State var count: Int
-  let limit: Int
-
-  var body: some View {
-    if count < limit {
-      VStack {
-        Button("Increment") { count += 1 }
-        Text("\(count)")
-      }
-      .onAppear { print("Counter.VStack onAppear") }
-      .onDisappear { print("Counter.VStack onDisappear") }
-    } else {
-      VStack { Text("Limit exceeded") }
+    @State var count: Int
+    let limit: Int
+    
+    var body: some View {
+        if count < limit {
+            VStack {
+                HTML("div", ["id": "testClass"]) {
+                    
+                    Button("Increment") { count += 1 }
+                    Text("\(count)")
+                }
+            }
+            .onAppear { print("Counter.VStack onAppear") }
+            .onDisappear { print("Counter.VStack onDisappear") }
+        } else {
+            VStack { Text("Limit exceeded") }
+        }
     }
-  }
 }
 
 
 struct MouseEventsView: View {
-  @State var position: CGPoint = .zero
-  @State var isMouseButtonDown: Bool = false
-
-  var body: some View {
-    DynamicHTML(
-      "div",
-      ["style": "width: 200px; height: 200px; background-color: red;"],
-      listeners: [
-        "mousemove": { event in
-          guard
-            let x = event.offsetX.jsValue.number,
-            let y = event.offsetY.jsValue.number
-          else { return }
-
-          position = CGPoint(x: x, y: y)
-        },
-        "mousedown": { _ in isMouseButtonDown = true },
-        "mouseup": { _ in isMouseButtonDown = false },
-      ]
-    ) {
-      Text("position is \(position), is mouse button down? \(isMouseButtonDown)")
+    @State var position: CGPoint = .zero
+    @State var isMouseButtonDown: Bool = false
+    
+    var body: some View {
+        HTML("div", ["id": "testClassDynamicHTML"]) {
+            DynamicHTML(
+                "div",
+                ["style": "width: 200px; height: 200px; background-color: red;"],
+                listeners: [
+                    "mousemove": { event in
+                        guard
+                            let x = event.offsetX.jsValue.number,
+                            let y = event.offsetY.jsValue.number
+                        else { return }
+                        
+                        position = CGPoint(x: x, y: y)
+                    },
+                    "mousedown": { _ in isMouseButtonDown = true },
+                    "mouseup": { _ in isMouseButtonDown = false },
+                ]
+            ) {
+                Text("position is \(position), is mouse button down? \(isMouseButtonDown)")
+            }
+        }
     }
-  }
 }
