83627219c24c98623764b2d301e3ab6345cbc7a4
Alex Jovmir <ajog@yandex.ru>
2023-12-04 20:07:15 +0100

    update custom script

--- custom.js
+++ custom.js
@@ -1,15 +1,8 @@
+show('_tokamak-stack _tokamak-hstack');
+
+
 function show(param_div_id) {
-    const main = document.getElementById('main_place');
+    const main = document.getElementById('body_for_swiftUI');
     const original = document.getElementsByClassName(param_div_id)[0];
     main.replaceWith(original);
-}
-
-if (document.readyState == 'complete') {
-    show('test_main_to_change');
-} else {
-    document.onreadystatechange = function () {
-        if (document.readyState === "complete") {
-            show('test_main_to_change');
-        }
-    }
 }
